7dc34531a8d8a7c1a64eb9a5a86680fff9a39479
mori618 <148190402+mori618@users.noreply.github.com>
2023-11-03 08:41:28 +0000

    raiout 11/3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
     @books = Book.all
@@ -8,9 +6,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to "/books/#{book.id}"
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -18,9 +20,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-  def destry
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book= Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
